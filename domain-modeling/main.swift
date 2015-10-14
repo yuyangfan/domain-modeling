c2770d5efcaaa15f876502be26939199cc966248
Tony Fan <yf8@uw.edu>
2015-10-13 23:15:28 -0700

    Initial Commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Yuyang Fan on 10/13/15.
+//  Copyright © 2015 Yuyang Fan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
